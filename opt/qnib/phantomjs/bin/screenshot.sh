b19f67ae443ab98258d2790982b49884c8dc637e
Christian Kniep <christian@qnib.org>
2016-04-13 15:27:51 +0200

    bugfix screenshot.sh

--- opt/qnib/phantomjs/bin/screenshot.sh
+++ opt/qnib/phantomjs/bin/screenshot.sh
@@ -10,12 +10,13 @@ if [ -z "${TS_ITER}" ];then
   TS_ITER=0
 fi
 CNT=0
+SCREEN_URL=$(echo ${SCREENSHOT_URL} |tr -d '"')
 for TS in ${TS_ITER};do
   while [ $(date +%s) -lt $(echo "${TS_START}+${TS}" |bc) ];do
     sleep 1
   done
   TIMESTAMP=$(date +"%F %H:%M:%S")
-  phantomjs /opt/qnib/phantomjs/bin/take_screenshot.js ${SCREENSHOT_URL} ${SCREENSHOT_FILE_PATH}/${SCREENSHOT_NAME}_${FILE_SUFFIX}_${CNT}_t${TS}.png
+  phantomjs /opt/qnib/phantomjs/bin/take_screenshot.js ${SCREEN_URL} ${SCREENSHOT_FILE_PATH}/${SCREENSHOT_NAME}_${FILE_SUFFIX}_${CNT}_t${TS}.png
   ## Annotate TIMESTAMP
   convert -fill blue -gravity SouthEast -pointsize 30 ${SCREENSHOT_FILE_PATH}/${SCREENSHOT_NAME}_${FILE_SUFFIX}_${CNT}_t${TS}.png -draw "text 5,5 'PIC TAKEN: ${TIMESTAMP}'" ${SCREENSHOT_FILE_PATH}/${SCREENSHOT_NAME}_${FILE_SUFFIX}_${CNT}_t${TS}.png
   convert -fill blue -gravity SouthEast -pointsize 30 ${SCREENSHOT_FILE_PATH}/${SCREENSHOT_NAME}_${FILE_SUFFIX}_${CNT}_t${TS}.png -draw "text 5,40 'DELAY: ${TS}sec'" ${SCREENSHOT_FILE_PATH}/${SCREENSHOT_NAME}_${FILE_SUFFIX}_${CNT}_t${TS}.png
