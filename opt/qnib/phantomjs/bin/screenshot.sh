960171b1c5c97a9e546044f942d80388df38d738
Christian Kniep <christian@qnib.org>
2016-04-14 17:37:44 +0200

    adjust delay for screenshot

--- opt/qnib/phantomjs/bin/screenshot.sh
+++ opt/qnib/phantomjs/bin/screenshot.sh
@@ -12,12 +12,14 @@ if [ -z "${TS_ITER}" ];then
 fi
 CNT=0
 SCREEN_URL=$(echo ${SCREENSHOT_URL} |tr -d '"')
+SCREEN_DELAY=${SCREENSHOT_DELAY=2000}
+
 for TS in ${TS_ITER};do
   while [ $(date +%s) -lt $(echo "${TS_START}+${TS}" |bc) ];do
     sleep 1
   done
   TIMESTAMP=$(date +"%F %H:%M:%S")
-  phantomjs /opt/qnib/phantomjs/bin/take_screenshot.js ${SCREEN_URL} ${SCREENSHOT_FILE_PATH}/${TIME_START}_t${TS}_${SCREENSHOT_NAME}.png ${SCREENSHOT_TOKEN}
+  phantomjs /opt/qnib/phantomjs/bin/take_screenshot.js ${SCREEN_URL} ${SCREENSHOT_FILE_PATH}/${TIME_START}_t${TS}_${SCREENSHOT_NAME}.png ${SCREEN_DELAY} ${SCREENSHOT_TOKEN}
   ## Annotate TIMESTAMP
   convert -fill blue -gravity SouthEast -pointsize 30 ${SCREENSHOT_FILE_PATH}/${TIME_START}_t${TS}_${SCREENSHOT_NAME}.png -draw "text 5,5 'PIC TAKEN: ${TIMESTAMP}'" ${SCREENSHOT_FILE_PATH}/${TIME_START}_t${TS}_${SCREENSHOT_NAME}.png
   convert -fill blue -gravity SouthEast -pointsize 30 ${SCREENSHOT_FILE_PATH}/${TIME_START}_t${TS}_${SCREENSHOT_NAME}.png -draw "text 5,40 'DELAY: ${TS}sec'" ${SCREENSHOT_FILE_PATH}/${TIME_START}_t${TS}_${SCREENSHOT_NAME}.png
--- opt/qnib/phantomjs/bin/take_screenshot.js
+++ opt/qnib/phantomjs/bin/take_screenshot.js
@@ -1,23 +1,26 @@
 var page = new WebPage(),
     system = require('system'),
     s_width = 1280,
-    s_height = 960;
+    s_height = 960,
+    screen_url = system.args[1],
+    shot_path = system.args[2],
+    screendelay = parseInt(system.args[3]);
 
 //viewportSize being the actual size of the headless browser
 page.viewportSize = { width: s_width, height: s_height };
 //the clipRect is the portion of the page you are taking a screenshot of
 page.clipRect = { top: 0, left: 0, width: s_width, height: s_height };
 // if a thrid arg is given
-if (system.args.length === 4) {
-    page.customHeaders={'Authorization': 'Bearer '+ system.args[3]};
+if (system.args.length === 5) {
+    page.customHeaders={'Authorization': 'Bearer '+ system.args[4]};
 }
-page.open(system.args[1], function (status) {
+page.open(screen_url, function (status) {
         just_wait();
 });
 
 function just_wait() {
     setTimeout(function() {
-            page.render(system.args[2]);
+            page.render(shot_path);
             phantom.exit();
-    }, 2000);
+    }, screendelay);
 }
